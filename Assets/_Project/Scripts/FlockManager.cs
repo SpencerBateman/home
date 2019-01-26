1162bfb87673afa6eb8b2eb9d39c10eb8cb64d73
Sarah Babski <sarah.b@iconinteractive.com>
2019-01-26 13:57:47 -0500

    initial sheep movement

---
+++ Assets/_Project/Scripts/FlockManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlockManager : MonoBehaviour
+{
+    public float minVelocity = 5;
+    public float maxVelocity = 20;
+    public float randomness = 1;
+    public int flockSize = 20;
+    public GameObject prefab;
+    //public GameObject chasee;
+
+    public Vector3 flockCenter;
+    public Vector3 flockVelocity;
+
+    private Walker[] walkers;
+
+    void Awake()
+    {
+        walkers = new Walker[flockSize];
+        for (var i = 0; i < flockSize; i++)
+        {
+            Vector3 position = new Vector3(Random.value * 2 - 1, Random.value * 2 - 1, Random.value * 2 - 1).normalized * 0.01f;
+
+            Walker w = Instantiate(prefab, transform.position, transform.rotation).GetComponent<Walker>();
+            w.transform.parent = transform;
+            w.transform.localPosition = position;
+            w.GetComponent<Walker>().SetManager(this);
+            walkers[i] = w;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 theCenter = Vector3.zero;
+        Vector3 theVelocity = Vector3.zero;
+
+        foreach (Walker w in walkers)
+        {
+            theCenter = theCenter + w.transform.localPosition;
+            theVelocity = theVelocity + w.GetComponent<Rigidbody>().velocity;
+        }
+
+        flockCenter = theCenter / (flockSize);
+        flockVelocity = theVelocity / (flockSize);
+    }
+}
+   
---
+++ Assets/_Project/Scripts/Walker.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Walker : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float acceleration = 9.8f;
+    private FlockManager manager;
+
+    private bool inited = false;
+    private float minVelocity;
+    private float maxVelocity;
+    private float randomness;
+    //private GameObject chasee;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = Vector3.zero;
+        rb.velocity = new Vector3(Random.value * 2 - 1, Random.value * 2 - 1, Random.value * 2 - 1);
+        rb.AddForce((manager.transform.position - transform.position).normalized * acceleration);
+    }
+
+    void FixedUpdate()
+    {
+        //rb.velocity = rb.velocity + Calc() * Time.deltaTime;
+        rb.AddForce((manager.transform.position - transform.position).normalized * acceleration);
+    }
+
+    IEnumerator Steering()
+    {
+        yield return null;
+        while (true)
+        {
+            rb.velocity = rb.velocity + Calc() * Time.deltaTime;
+
+            float waitTime = Random.Range(0.3f, 0.5f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    public void SetManager(FlockManager flockManager)
+    {
+        manager = flockManager;
+        minVelocity = manager.minVelocity;
+        maxVelocity = manager.maxVelocity;
+        randomness = manager.randomness;
+        StartCoroutine("Steering");
+    }
+
+    private Vector3 Calc()
+    {
+        Vector3 randomize = new Vector3((Random.value * 2) - 1, (Random.value * 2) - 1, (Random.value * 2) - 1);
+
+        randomize.Normalize();
+        Vector3 flockCenter = manager.flockCenter;
+        Vector3 flockVelocity = manager.flockVelocity;
+
+        flockCenter = flockCenter - transform.localPosition;
+        flockVelocity = flockVelocity - rb.velocity;
+
+        return (flockCenter + flockVelocity + randomize * randomness);
+    }
+}
