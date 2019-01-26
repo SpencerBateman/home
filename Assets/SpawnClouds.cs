da566405d2fc8bd4edd3e0b1007bbaef347289e5
Spencer Bateman <bateman.s@husky.neu.edu>
2019-01-26 13:48:09 -0500

    cloud orbit

---
+++ Assets/SpawnClouds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClouds : MonoBehaviour {
+
+  // The list of Orbiting Objects to Spawn
+  public GameObject[] OrbitingObjects;
+
+  // Initialize and spawn
+  void Start() {
+    foreach (GameObject g in OrbitingObjects) {
+      GameObject childObject = Instantiate(g) as GameObject;
+      childObject.transform.parent = gameObject.transform;
+      childObject.transform.position = new Vector3(.1f, 0, 0);
+    }
+  }
+}
